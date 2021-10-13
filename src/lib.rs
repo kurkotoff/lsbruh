ab629b88a50cac4c14864513f64be34499028f3c
kurkotoff <kurkotoff2@gmail.com>
2021-10-14 01:11:11 +0300

    reworked command line reading structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,46 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::BufWriter;
-use std::io::Read;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-use png::Decoder;
-use png::Encoder;
-
 #[derive(StructOpt, Debug)]
 pub struct Config {
-    /// Output file
-    #[structopt(short, long, parse(from_os_str))]
-    output: PathBuf,
+    #[structopt(subcommand)]
+    pub cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+pub enum Command {
+    Write(WriteOptions),
+}
+
+#[derive(Debug, StructOpt)]
+pub struct WriteOptions {
+    /// Input file to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    pub secret: PathBuf,
 
     /// Message to hide
     #[structopt(
         short = "-f", 
         long = "--input-file",
         parse(from_os_str))]
-    input_file: PathBuf,
+    pub input_file: PathBuf,
 
-    /// Input file to process
-    #[structopt(name = "FILE", parse(from_os_str))]
-    secret: PathBuf,
+    /// Output file
+    #[structopt(short, long, parse(from_os_str))]
+    pub output: PathBuf,
 }
 
-/*
-  Config is a structure that saves
-  the command line arguments in their
-  respective values
-*/
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    
-    // Reading message from config and converting it into bits
-    let mut input_file = File::open(&config.input_file)
-        .expect(format!("No file {:?} found", config.input_file).as_str());
+// /*
+//   Converts a u8 number into an array
+//   of 0 and 1 with length 8
 
-    let mut message: Vec<u8> = Vec::new();
-    
-    input_file.read_to_end(&mut message).unwrap();
-    
-    let bin_bytes = bin_bytes(&message);
-    
-    // Creating the decoder and reader objects 
-    // to read input image data
-    let decoder = Decoder::new(File::open(&config.secret).unwrap());
-    let mut reader = decoder.read_info().unwrap();
+//   EXAMPLE:
+//     123 => [0, 1, 1, 1, 1, 0, 1, 1]
+//     10  => [0, 0, 0, 0, 1, 0, 1, 0]
+// */
 
-    // Creating the input image data buffer
-    let mut data = vec![0; reader.output_buffer_size()];
-    
-    // Saving the image data as bytes (u8)
-    reader.next_frame(&mut data).unwrap();
-
-    // Pulling information about image to an object
-    let info = reader.info();
-
-    encode_lsb(&mut data, &bin_bytes);
-
-    // Creating an encoder and writer objects
-    // For generating an output pic
-    let output_file = File::create(PathBuf::from(&config.output)).unwrap();
-    let mut encoder = Encoder::new(BufWriter::new(output_file), info.width, info.height);
-
-    encoder.set_color(info.color_type);
-    encoder.set_depth(info.bit_depth);
-
-    let mut writer = encoder.write_header().unwrap();
-
-    // Writing the processed data to a new image
-    writer.write_image_data(&data).unwrap();
-
-    Ok(())
-}
-
-/*
-  Converts a u8 number into an array
-  of 0 and 1 with length 8
-
-  EXAMPLE:
-    123 => [0, 1, 1, 1, 1, 0, 1, 1]
-    10  => [0, 0, 0, 0, 1, 0, 1, 0]
-*/
-
-fn bin_u8(num: &u8) -> Vec<u8> {
+pub fn bin_u8(num: &u8) -> Vec<u8> {
     let mut res: Vec<u8> = Vec::new();
     let mut num = *num;
 
@@ -99,15 +54,15 @@ fn bin_u8(num: &u8) -> Vec<u8> {
     res
 }
 
-/*
-  Converts an array of bytes into an array
-  of their binary representation
+// /*
+//   Converts an array of bytes into an array
+//   of their binary representation
 
-  EXAMPLE:
-    [123, 10] => [0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0]
-*/
+//   EXAMPLE:
+//     [123, 10] => [0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0]
+// */
 
-fn bin_bytes(bytes: &[u8]) -> Vec<u8> {
+pub fn bin_bytes(bytes: &[u8]) -> Vec<u8> {
     let mut res: Vec<u8> = Vec::new();
 
     for byte in bytes {
@@ -131,7 +86,7 @@ fn bin_bytes(bytes: &[u8]) -> Vec<u8> {
     10  => 0000101|0| => 0000101|1|
 */
 
-fn encode_lsb(file_data: &mut Vec<u8>, message_bits: &Vec<u8>) {
+pub fn encode_lsb(file_data: &mut Vec<u8>, message_bits: &Vec<u8>) {
     for i in 0..message_bits.len() {
         file_data[i] >>= 1;
         file_data[i] <<= 1;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,59 @@
-use lsbruh::Config;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use std::io::BufWriter;
+
 use structopt::StructOpt;
+
+use png::{Decoder, Encoder};
+
+use lsbruh::Command;
+use lsbruh::Config;
 
 
 fn main() {
     let config = Config::from_args();
     
-    if let Err(e) = lsbruh::run(config) {
-        eprintln!("Application error: {}", e);
+    match config.cmd {
+        Command::Write(options) => {
+                // Reading message from config and converting it into bits
+                let mut input_file = File::open(&options.input_file)
+                    .expect(format!("No file {:?} found", options.input_file).as_str());
 
-        std::process::exit(1);
+                let mut message: Vec<u8> = Vec::new();
+                
+                input_file.read_to_end(&mut message).unwrap();
+                
+                let bin_bytes = lsbruh::bin_bytes(&message);
+                
+                // Creating the decoder and reader objects 
+                // to read input image data
+                let decoder = Decoder::new(File::open(&options.secret).unwrap());
+                let mut reader = decoder.read_info().unwrap();
+
+                // Creating the input image data buffer
+                let mut data = vec![0; reader.output_buffer_size()];
+                
+                // Saving the image data as bytes (u8)
+                reader.next_frame(&mut data).unwrap();
+
+                // Pulling information about image to an object
+                let info = reader.info();
+
+                lsbruh::encode_lsb(&mut data, &bin_bytes);
+
+                // Creating an encoder and writer objects
+                // For generating an output pic
+                let output_file = File::create(PathBuf::from(&options.output)).unwrap();
+                let mut encoder = Encoder::new(BufWriter::new(output_file), info.width, info.height);
+
+                encoder.set_color(info.color_type);
+                encoder.set_depth(info.bit_depth);
+
+                let mut writer = encoder.write_header().unwrap();
+
+                // Writing the processed data to a new image
+                writer.write_image_data(&data).unwrap();
+        }
     }
 }
